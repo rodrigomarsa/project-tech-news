b05de7907171104089c5b413981987195b7cbbde
Rodrigo <rodrigomartins.agro@gmail.com>
2023-07-28 14:08:14 -0300

    feat: add search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -9,8 +10,12 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+        news_by_date = search_news({"timestamp": date})
+        return [(new["title"], new["url"]) for new in news_by_date]
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
