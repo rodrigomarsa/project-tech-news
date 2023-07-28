20a2a61f7206cbb7a4fa1ec9c5abcd8451bee4fd
Rodrigo <rodrigomartins.agro@gmail.com>
2023-07-28 15:22:28 -0300

    feat: add ratings.py

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,12 @@
+from tech_news.database import db
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    categories = db.news.aggregate(
+        [
+            {"$group": {"_id": "$category", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1, "_id": 1}},
+        ]
+    )
+    return [category["_id"] for category in categories][:5]
